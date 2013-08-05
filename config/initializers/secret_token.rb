eec8b81aa40fbae20dc718f9c41b7f65aeb2f7b4
Seva <vabaskin@gmail.com>
2013-08-05 11:28:49 +0100

    generated rails application; added spec folder; added capybara & rails/rspec to Gemfile

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MadBid::Application.config.secret_key_base = 'c2d2d8c7b6f8dad64c54a153d46c96b7d27c1a579d317f18814b7d8e54433d4034ce2faf9db85e6fcef8c2873cdd7f75a6ff2e5c949d22956f627c30f8962423'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MadBid::Application.config.session_store :cookie_store, key: '_mad_bid_session'
