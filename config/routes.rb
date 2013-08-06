2bda389e8e0210e0124e94ab599ccd8418727aac
Seva <vabaskin@gmail.com>
2013-08-06 14:23:35 +0100

    Integrated Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 MadBid::Application.routes.draw do
+
+  root "welcome#index"
+  devise_for :admins
+    
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
