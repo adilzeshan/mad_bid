d44e060a59371ae5f49a1c19fcbf3c30786b1193
Adil Zeshan <adil@zeshan.org>
2013-08-07 15:27:53 +0100

    Added bid model

--- spec/models/listing_spec.rb
+++ spec/models/listing_spec.rb
@@ -1,5 +1,21 @@
 require 'spec_helper'
 
 describe Listing do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+let(:listing) {Factory.build(:listing)}
+let(:bid) {Factory.build(:bid)}
+let(:user) {Factory.build(:user)}
+
+  context 'active status' do 
+  it 'is currently queued and not active yet'
+  it 'is now active'
+  end
+
+  context 'bidding' do
+  it 'bidding increments cost of listing'
+  end
+
+  context 'timer'
+
+  context ''
 end
