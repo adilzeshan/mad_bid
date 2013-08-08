91f8833d0ca05f169c62a9d5e1c260d618651b29
Adil Zeshan <adil@zeshan.org>
2013-08-08 15:48:51 +0100

    Removed unnecessary comments from users model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,3 @@
-    # t.string   "email",                  default: "", null: false
-    # t.string   "encrypted_password",     default: "", null: false
-    # t.string   "reset_password_token"
-    # t.datetime "reset_password_sent_at"
-    # t.datetime "remember_created_at"
-    # t.integer  "sign_in_count",          default: 0
-    # t.datetime "current_sign_in_at"
-    # t.datetime "last_sign_in_at"
-    # t.string   "current_sign_in_ip"
-    # t.string   "last_sign_in_ip"
-    # t.datetime "created_at"
-    # t.datetime "updated_at"
-
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
