a8787894e9437ada161379d85c7be0f0222ad53e
Adil Zeshan <adil@zeshan.org>
2013-08-09 16:30:54 +0100

    Updated bids model and its rspec

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -2,4 +2,9 @@ class Bid < ActiveRecord::Base
 
 	  belongs_to :user
       belongs_to :listing
+
+      def update_price before
+      	self.update(before_price: before)
+      	self.update(after_price: before + 1)
+      end
 end
--- spec/models/bid_spec.rb
+++ spec/models/bid_spec.rb
@@ -1,5 +1,28 @@
 require 'spec_helper'
 
 describe Bid do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+	# let(:macbook) {double :listing, current_price: '£20.00', cost_per_bid: '£0.50'}
+	# let(:user) {double :user}
+
+	context 'a newly placed bid' do
+
+		before(:each) do
+			user = mock_model("User")
+			@macbook = mock_model("Listing")
+			@macbook.stub(current_price: 20).stub(cost_per_bid: 1)
+
+			@my_bid = Bid.create user: user, listing: @macbook
+		end
+
+		it 'applies to a current price before bid is made' do
+			@my_bid.update_price @macbook.current_price
+			expect(@my_bid.before_price).to eq 20
+		end
+
+		it 'results in a new price after the bid is made' do
+			@my_bid.update_price @macbook.current_price
+			expect(@my_bid.after_price).to eq 21
+		end
+	end
 end
