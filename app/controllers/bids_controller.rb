07b9e4f47a9f2b74ccba549bc800b14a9b087611
Seva <vabaskin@gmail.com>
2013-08-14 18:04:16 +0100

    removed extra end keyword in bids controller

--- app/controllers/bids_controller.rb
+++ app/controllers/bids_controller.rb
@@ -1,23 +1,20 @@
 class BidsController < ApplicationController
   def create
-  if current_user
+    if current_user
       # verify that params['id'] is within the range
       # of ids of the active listings. Or catch an exception
       # => find will return a massive exceptiom
       # => find_by will return a nil.
-   @listing = Listing.find(params['id'].to_i)
+    @listing = Listing.find(params['id'].to_i)
    # puts params['id'].to_i
    # puts @listing.to_s
-   current_user.makes_bid @listing
+      current_user.makes_bid @listing
   #else
   # if post successful
   #flash, login popup?
 
-    #WebsocketRails[:bids].trigger 'new',
-    #  { id: @listing.id, current_price: @listing.current_price, latest_bidder: @listing.latest_bidder, active: @listing.active }
      websocket[:bids].trigger 'new',
        { id: @listing.id, current_price: @listing.current_price, latest_bidder: @listing.latest_bidder, active: @listing.active }
   	end
-  end
   end
 end
