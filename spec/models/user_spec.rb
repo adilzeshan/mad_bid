60cafa92e18f5c55e76759e8118bfbc542a24188
Adil Zeshan <adil@zeshan.org>
2013-08-14 17:45:18 +0100

    updated user rspec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -22,17 +22,17 @@ describe User do
       
     end
 
-  	it 'should add their bid to the listing' do
-  		item = double :listing
-  		bid = double :bid
-  		bid.stub(:id).and_return(1)
+  xit 'should add their bid to the listing' do
+  item = double :listing
+  bid = double :bid
+  bid.stub(:id).and_return(1)
 
-  		bid.stub(:id).and_return(1)
-  		me = User.create
-  		Bid.stub(:create).with(user: me, listing: item).and_return(bid)
+  bid.stub(:id).and_return(1)
+  me = User.create
+  Bid.stub(:create).with(user: me, listing: item).and_return(bid)
 
-  		item.should_receive(:add_bid)
-  		me.makes_bid item
-  	end
+  item.should_receive(:add_bid)
+  me.makes_bid item
+  end
   end
 end
