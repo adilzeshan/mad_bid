a5b0417bf2ce7a432e439085837285881729197f
Adil Zeshan <adil@zeshan.org>
2013-08-15 18:03:32 +0100

    Removed some comments from bids_controller

--- app/controllers/bids_controller.rb
+++ app/controllers/bids_controller.rb
@@ -6,15 +6,13 @@ class BidsController < ApplicationController
       # => find will return a massive exceptiom
       # => find_by will return a nil.
     @listing = Listing.find(params['id'].to_i)
-   # puts params['id'].to_i
-   # puts @listing.to_s
-      current_user.makes_bid @listing
+    current_user.makes_bid @listing
   #else
   # if post successful
   #flash, login popup?
 
      websocket[:bids].trigger 'new',
        { id: @listing.id, current_price: @listing.current_price, latest_bidder: @listing.latest_bidder, active: @listing.active }
-  	end
+    end
   end
 end
