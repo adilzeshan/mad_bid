61ededebb956d57e097c3960117dcc2e6400e3a0
Seva <vabaskin@gmail.com>
2013-08-18 17:07:45 +0100

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 MadBid::Application.routes.draw do
 
-  get "charges/new"
-  get "charges/create"
+  #get "charges/new"
+  #get "charges/create"
   post "bids/create"
   get "home/index"
   get "admins_pannel/index"
   devise_for :users
   resources :listings do
   end
-  resources :charges
+  resources :charges, :only => [:index, :new, :create]
 
   root "home#index"
   devise_for :admins
